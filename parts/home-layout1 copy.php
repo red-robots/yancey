af2f9a5312764fb970a8bd9c41d69b28b187f769
Lisa <lisaqdebona@gmail.com>
2025-09-09 02:41:37 -0400

    Styled buttons

--- parts/home-layout1 copy.php
+++ parts/home-layout1 copy.php
@@ -19,26 +19,30 @@
           <div class="images">
           <?php foreach ($images as $img) { 
             $imgsrc = $img['image'];
-            $imgTitle = $img['title'];
-            $description = $img['description'];
             $imgUrl = (isset($imgsrc['url']) && $imgsrc['url']) ? $imgsrc['url'] : '';
-            // $link = $img['link'];
-            // $imgTitle = ( isset($link['title']) && $link['title'] ) ? $link['title'] : '';
-            // $imgLink = ( isset($link['url']) && $link['url'] ) ? $link['url'] : '';
-            // $imgTarget = ( isset($link['target']) && $link['target'] ) ? $link['target'] : '';
+            $link = $img['link'];
+            $imgTitle = ( isset($link['title']) && $link['title'] ) ? $link['title'] : '';
+            $imgLink = ( isset($link['url']) && $link['url'] ) ? $link['url'] : '';
+            $imgTarget = ( isset($link['target']) && $link['target'] ) ? $link['target'] : '';
             if($imgUrl) { ?>
-              <figure tabindex="0" class="<?php echo ($description) ? 'hasHover':'noHover' ?>">
-                <div class="inner">
-                  <?php if ($imgTitle) { ?>
-                  <div class="imageTitle"><?php echo $imgTitle ?></div>
-                  <?php } ?>
-                  <?php if ($description) { ?>
-                  <div class="imageText">
-                    <div class="wrap"><?php echo anti_email_spam($description); ?></div>
+              <figure>
+                <?php if ($imgLink && $imgLink!='#') { ?>
+                <a href="<?php echo $imgLink ?>" target="<?php echo $imgTarget ?>" class="imagelink">
+                  <div class="inner">
+                    <?php if ($imgTitle) { ?>
+                    <span class="imageText"><?php echo $imgTitle ?></span>  
+                    <?php } ?>
+                    <img src="<?php echo $imgUrl ?>" alt="<?php echo $imgTitle ?>">
                   </div>
-                  <?php } ?>
-                  <img src="<?php echo $imgUrl ?>" alt="<?php echo $imgTitle ?>">
-                </div> 
+                </a>  
+                <?php } else { ?>
+                  <div class="inner">
+                    <?php if ($imgTitle) { ?>
+                    <span class="imageText"><?php echo $imgTitle ?></span>  
+                    <?php } ?>
+                    <img src="<?php echo $imgUrl ?>" alt="<?php echo $imgTitle ?>">
+                  </div>
+                <?php } ?>
               </figure>
             <?php } ?>
           <?php } ?>
