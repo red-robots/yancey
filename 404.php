7978bc3592acb1d76e9c645824dee6a288556175
Mindy <mindy@valhallaonlineservices.com>
2025-09-25 21:51:51 +0800

    Format page 404

--- 404.php
+++ 404.php
@@ -11,20 +11,21 @@ get_header(); ?>
 
 <div id="primary" class="content-area-full generic-layout page404-content">
   <main id="main" class="site-main" role="main">
-
-      <div class="titlediv">
-        <div class="wrapper"><h1 class="page-title"><?php esc_html_e( 'Page Not Found!', 'bellaworks' ); ?></h1></div>
-      </div>
-      
-      
-      <div class="entry-content contentDiv">
-        <div class="wrapper">
-          <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below.', 'bellaworks' ); ?></p>
-          <div id="sitemap-wrap">
-            <?php wp_nav_menu( array( 'theme_location' => 'sitemap', 'menu_id' => 'sitemap','container_class'=>'sitemap-links') ); ?>
+    <section class="repeatable_layout2">
+      <div class="wrapper">
+        <div class="flexwrap">
+          <div class="titlediv">
+            <h1 class="page-title"><?php esc_html_e( 'Page Not Found!', 'bellaworks' ); ?></h1>
+          </div>
+          <div class="entry-content contentDiv">
+            <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below.', 'bellaworks' ); ?></p>
+            <div id="sitemap-wrap">
+              <?php wp_nav_menu( array( 'theme_location' => 'sitemap', 'menu_id' => 'sitemap','container_class'=>'sitemap-links') ); ?>
+            </div>
           </div>
         </div>
       </div>
+    </section>
   </main>
 </div>
 
